7580396df0352e4a5296d71728d63306e36c198b
pUrGe12 <helloitsme0218@gmail.com>
2025-01-08 02:25:49 +0530

    please

--- src/backend/models/extraction.py
+++ src/backend/models/extraction.py
@@ -6,7 +6,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__))) # adding the root directory to path
-from backend.prompts import extractionPrompt
+from prompts import extractionPrompt
 
 # from dotenv import load_dotenv
 from pathlib import Path
